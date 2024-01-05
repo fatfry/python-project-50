c84ed0ea652f80540fd387837347f6e210edd6d1
oleg <fatfry@mail.ru>
2024-01-05 14:03:58 +0300

    Rebase logic and print diff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-import argparse
+from gendiff.cli import parse_args
+from gendiff.gendiff_build import gendiff_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--flag', help='set format of output')
-    args = parser.parse_args()
+    args = parse_args()
+    print(gendiff_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
