6773e2a37a42dccc14367737e1424cfa3967f22f
oleg <fatfry@mail.ru>
2024-01-05 14:04:47 +0300

    Rebase logic to build diff

--- gendiff/gendiff_build.py
+++ gendiff/gendiff_build.py
@@ -1,18 +1,28 @@
 import json
 
 
+def to_str(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    else:
+        return value
+
+
 def gendiff_diff(file1, file2):
     data1 = json.load(open(file1))
     data2 = json.load(open(file2))
 
     keys = sorted(data1.keys() | data2.keys())
+    result = []
     for key in keys:
         if key not in data2:
-            print(f'- {key}: {data1[key]}')
+            result.append(f'- {key}: {to_str(data1[key])}')
         elif key not in data1:
-            print(f'+ {key}: {data2[key]}')
+            result.append(f'+ {key}: {to_str(data2[key])}')
         elif data1[key] != data2[key]:
-            print(f'- {key}: {data1[key]}')
-            print(f'+ {key}: {data2[key]}')
+            result.append(f'- {key}: {to_str(data1[key])}')
+            result.append(f'+ {key}: {to_str(data2[key])}')
         else:
-            print(f'  {key}: {data1[key]}')
+            result.append(f'  {key}: {to_str(data1[key])}')
+    result = "\n  ".join(result)
+    return f'{{\n  {result}\n}}'
