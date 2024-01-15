066e063e9e1e2f4f36c130a33a051264853f9957
oleg <fatfry@mail.ru>
2024-01-15 22:05:37 +0300

    Created file diff_yml

---
+++ gendiff/diff_yml.py
@@ -0,0 +1,23 @@
+import os
+from gendiff_build import gendiff_diff
+from parser import parse
+
+
+def gendiff(file1, file2):
+    data1 = get_file_data(file1)
+    data2 = get_file_data(file2)
+    return gendiff_diff(data1, data2)
+
+
+def get_file_data(file_path):
+    extension = get_file_extension(file_path)
+    return parse(open(file_path), extension)
+
+
+def get_file_extension(file_path):
+    _, extension = os.path.splitext(file_path)
+    return extension[1:]
+
+
+print(gendiff('/home/oleg/python-project-50/tests/fixtures/file2.json',
+              '/home/oleg/python-project-50/tests/fixtures/file1.yml'))
