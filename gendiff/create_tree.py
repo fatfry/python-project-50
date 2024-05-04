b81e363f870f924a67849c21fbd4159090905781
oleg <fatfry@mail.ru>
2024-05-04 21:22:18 +0300

    fix up

--- gendiff/create_tree.py
+++ gendiff/create_tree.py
@@ -6,9 +6,11 @@ def create_difference_tree(data1, data2):
     result = {}
     for key in keys:
         if key not in data2:
-            result[key] = {'type': CHANGES_FORMATS.REMOVED, 'value': data1[key]}
+            result[key] = {'type': CHANGES_FORMATS.REMOVED,
+                           'value': data1[key]}
         elif key not in data1:
-            result[key] = {'type': CHANGES_FORMATS.ADDED, 'value': data2[key]}
+            result[key] = {'type': CHANGES_FORMATS.ADDED,
+                           'value': data2[key]}
 
         elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
             result[key] = {
@@ -18,8 +20,10 @@ def create_difference_tree(data1, data2):
 
         elif data1[key] != data2[key]:
             result[key] = {'type': CHANGES_FORMATS.UPDATED,
-                           'old_value': data1[key], 'new_value': data2[key]}
+                           'old_value': data1[key],
+                           'new_value': data2[key]}
         else:
-            result[key] = {'type': CHANGES_FORMATS.UNCHANGED, 'value': data1[key]}
+            result[key] = {'type': CHANGES_FORMATS.UNCHANGED,
+                           'value': data1[key]}
 
     return result
